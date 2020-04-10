ae3c1a9e2cb3c08604af820265cd12d462d1f5ff
Florin <oancea.quentin.florin@gmail.com>
2020-04-10 13:45:16 +0300

    set get method with HttpClient

--- http-basic/src/app/app.component.ts
+++ http-basic/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,27 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'http-basic';
+  titles: any = ['lorem 1', 'lorem 2', 'lorem 3', 'lorem 4'];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getData() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(response => {
+      this.titles = response.slice(0, 10).map(rsp => rsp.title);
+    });
+  }
+
+  postData() {
+    // to post data
+  }
+
+  updateData() {
+    // to update data
+  }
+
+  deleteData() {
+    // to delete data
+  }
 }
--- http-basic/src/app/app.module.ts
+++ http-basic/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
